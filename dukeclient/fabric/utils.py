83d8db2785972ef85d498a0c1c619da88fe3836c
Maxime Haineault <max@motion-m.ca>
2012-03-27 16:20:49 -0400

    Added generic (as possible) fabric tasks

---
+++ dukeclient/fabric/utils.py
@@ -0,0 +1,58 @@
+import os
+
+
+def get_role(env):
+    for role in env.roles:
+        if env.host_string in env.roledefs[role]:
+            return role
+
+
+def get_conf(env, key, default=None):
+    if not hasattr(env, 'conf'):
+        env.name  = get_role(env)
+        env.hosts = env.site[env.name]['hosts']
+        env.conf  = env.site[env.name]
+
+    try:
+        v = env.conf[key]
+    except:
+        return None
+
+    if isinstance(v, list):
+        o = []
+        for r in v:
+            o.append(r % env.site)
+        return o
+    else:
+        if isinstance(v, str):
+            return v % env.site
+        else:
+            return v
+
+
+def get_project_path(env):
+    docroot = get_conf(env, 'document-root')
+    return os.path.join(docroot, env.site['package'])
+
+
+def event(env, name):
+    ctx = {
+        'package': env.site['package'],
+        'project': env.site['project'],
+        'domain':  env.site['domain'],
+    }
+    ev = get_conf(env, name)
+    if ev:
+        for cmd in ev:
+            sudo(cmd % ctx)
+
+
+def duke_init(env):
+    project_path = get_project_path(env)
+    venv_root = os.path.join(get_conf(env, 'document-root'), 'virtualenv', env.site['project'])
+    with cd(project_path):
+        if get_conf(env, 'virtualenv'):
+            pythonbin = os.path.join(venv_root, 'bin/python')
+            sudo('duke init %s -n --python=%s' % (env.site['project'], pythonbin))
+        else:
+            sudo('duke init %s -n' % env.site['project'])
