bbb9946d7b3e1819c4c6606f8ce60c0774fc0d5f
Krozark <maxime.barbier1991@gmail.com>
2012-06-07 16:52:59 -0400

    add admin.py

---
+++ dukeclient/templates/startapp/admin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from django.conf import settings
+from django.contrib import admin
+#from django.utils.translation import ugettext_lazy as _
+#from webcore.utils.admin import AdminThumbnailMixin
+#from grappellifit.admin import TranslationAdmin
+from %(project_name)s.models import *
+
+
