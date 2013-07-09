f0c07a8448e0d36c3cee49e47c00b4824af47c4b
Maxime Haineault <max@motion-m.ca>
2013-07-09 00:28:07 -0400

    Added django-devserver to dev.cfg, big code cleanup, refactored project initialization, moved src/ to .duke/src/, moved default eggs and download dirs to .duke/ (fixing the package gone after reboot)

--- dukeclient/commands/init.py
+++ dukeclient/commands/init.py
@@ -1,17 +1,20 @@
-import os, sys
+import os
 import dukeclient
 
 from dukeclient.commands import BaseCommand
 from dukeclient.commands.dev import DevCommand
+
 
 class InitCommand(BaseCommand):
     """
     Initialize duke for an existing project.
     """
 
+    bootstrap_opts = ' -v 1.7.1'
+
     options = [
         ('-b', '--base-path', {
-            'dest': 'base_path', 
+            'dest': 'base_path',
             'help': 'Directory where the project should be initialized.'}),
         ('-d', '--distribute', {
             'dest': 'distribute', 'action': 'store_true', 'default': False,
@@ -20,91 +23,107 @@ class InitCommand(BaseCommand):
             'dest': 'nodev', 'action': 'store_true', 'default': False,
             'help': 'Do not launch development env once init is complete'}),
         ('-p', '--python', {
-            'dest': 'python', 
-            'help': 'Python version to use (defaults to system default). Ex: python2.7'}),
+            'dest': 'python',
+            'help': 'Python version to use (defaults\
+ to system default). Ex: python2.7'}),
         ('-q', '--quiet', {
+            'action': 'store_true',
             'dest': 'quiet', 'default': False,
             'help': 'No output'}),
     ]
 
-    def call(self, *args, **options):
-        if len(args) < 1:
-            self.error("usage: duke init <project> [options]\n")
+    def setup_paths(self):
+        self.duke_path = os.path.join(self.base_path, '.duke/')
+        self.eggs_path = os.path.join(self.duke_path, 'cache/eggs/')
+        self.download_path = os.path.join(self.duke_path, 'cache/download/')
+        self.bin_path = os.path.join(self.duke_path, 'bin/')
+        self.conf_path = os.path.join(os.getenv("HOME"), '.duke/')
 
-        # FIXME: Validate for proper python module name
-        project_name = args[0].replace('/', '').replace('-', '_')
+        # create directories if needed
+        for path in ['base', 'duke', 'eggs', 'download', 'bin', 'conf']:
+            p = getattr(self, '%s_path' % path, None)
+            if p and not os.path.exists(p):
+                os.makedirs(p)
 
-        self.base_path  = 'base_path' in options and options['base_path'] or os.getcwd()
-        self.duke_path  = os.path.join(self.base_path, '.duke/')
-        self.bin_path   = os.path.join(self.duke_path, 'bin/')
-        self.conf_path  = os.path.join(os.getenv("HOME"), '.duke/')
-        self.cache_path = '/tmp/duke-client-cache/'
-
-        if not os.path.exists(self.duke_path):
-            os.makedirs(self.duke_path)
-
-        if not os.path.exists(self.conf_path):
-            os.mkdir(self.conf_path)
-            
-        if not os.path.exists(self.cache_path):
-            os.mkdir(self.cache_path)
-
+    def get_context(self):
         # Variables passed to generated .cfg files
-        context = {
+        return {
             'duke_client_version': dukeclient.VERSION,
-            'project_name': project_name,
-            'parent_dirname': os.path.basename(os.path.abspath(self.base_path)),
+            'project_name': self.project_name,
+            'parent_dirname': self.parent_dir,
             'base_path': self.base_path,
             'duke_path': self.duke_path,
+            'eggs_path': self.eggs_path,
+            'download_path': self.download_path,
             'settings_module': 'settings',
         }
 
-        #if not os.path.exists(os.path.join(self.conf_path, 'duke_conf.yml')):
-        #    self.info("Creating default global configuration file")
-        #    self.install_file('duke_conf.yml', self.conf_path, context)
-
+    def install_files(self):
         self.install_file('bootstrap.py', self.base_path)
-        self.install_file('buildout.cfg', self.base_path, context)
-        self.install_file('base.cfg', self.duke_path, context)
-       #self.install_file('python27.cfg', self.duke_path, context)
-        self.install_file('dev.cfg', self.base_path, context)
+        self.install_file('buildout.cfg', self.base_path, self.context)
+        self.install_file('base.cfg', self.duke_path, self.context)
+        self.install_file('dev.cfg', self.base_path, self.context)
 
-        if not options['quiet']:
+    def install_dev_hooks(self):
+        # Dev source
+        if not self.call_opts['quiet']:
+            self.info("Installing dev hooks")
+
+        self.install_file('dev', self.bin_path, self.context)
+        self.install_file('env', self.bin_path, self.context)
+        self.install_file('profile', self.bin_path, self.context)
+
+    def install_project_confs(self):
+        # The project_conf.yml might have local modifications so we want
+        # to preserve it.
+        conf_path = os.path.join(self.duke_path, 'project_conf.yml')
+        if not os.path.exists(conf_path):
+            self.install_file('project_conf.yml', self.duke_path, self.context)
+
+    def initialize_buildout(self):
+        if not self.call_opts['quiet']:
             self.info("Initializing zc.buildout")
-        
-        # Temporarely forcing bootstrap 1.7.1 since 2.0 seems to breaks everything it can break
-        boot_opts = ' -v 1.7.1'
-        python = options['python'] or 'python' # and python !
 
         # Even though distribute is considered superior and better maintained
         # than setuptools, it is disabled by default because I cannot get the
         # project sandboxing to work properly with it .. bummer.
-        if options['distribute']:
-            if not options['quiet']:
+        if self.call_opts['distribute']:
+            if not self.call_opts['quiet']:
                 self.info("Using distribute")
-            opts = ' -d'
+            self.bootstrap_opts = ' '.join([self.bootstrap_opts, '-d'])
 
-        status, output = self.local('%s bootstrap.py%s' % (python, boot_opts))
-        self.debug(output)
+        # Temporarely forcing bootstrap 1.7.1
+        # since 2.0 seems to breaks everything it can break
+        python = self.call_opts['python'] or 'python'  # and python !
 
-        # Dev source
-        if not options['quiet']:
-            self.info("Installing dev hooks")
+        command = '%s bootstrap.py%s' % (python, self.bootstrap_opts)
+        status, output = self.local(command)
 
-        if not os.path.exists(self.bin_path):
-            os.makedirs(self.bin_path)
+        if not self.call_opts['quiet']:
+            self.debug(output)
 
-        self.install_file('dev', self.bin_path, context)
-        self.install_file('env', self.bin_path, context)
-        self.install_file('profile', self.bin_path, context)
+    def call(self, *args, **options):
+        self.call_opts = options
+        if len(args) < 1:
+            self.error("usage: duke init <project> [options]\n")
 
-        # The project_conf.yml might have local modifications so we want
-        # to preserve it.
-        if not os.path.exists(os.path.join(self.duke_path, 'project_conf.yml')):
-            self.install_file('project_conf.yml', self.duke_path, context)
+        # FIXME: Validate for proper python module name
+        self.project_name = args[0].replace('/', '').replace('-', '_')
+        self.base_path = 'base_path' in options and options['base_path']\
+            or os.getcwd()
+        self.parent_dir = os.path.basename(os.path.abspath(self.base_path))
 
-        if not options['quiet']:
-            self.info("\nDone! Now type \"buildout\" to build the environment and install requirements.\n")
+        self.setup_paths()
+        self.context = self.get_context()
+
+        self.install_files()
+        self.initialize_buildout()
+        self.install_dev_hooks()
+        self.install_project_confs()
+
+        if not self.call_opts['quiet']:
+            self.info("\nDone! Now type \"buildout\"\
+to build the environment and install requirements.\n")
 
         if not options['nodev']:
             DevCommand().call()
